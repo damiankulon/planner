d0146747bb8aeb0c46797efbbcd472d3bf7f867e
damiankulon <damian.kulon@gmail.com>
2024-03-18 19:50:54 +0100

    Change events

--- backend/src/DaysOff/BusinessLogic.ts
+++ backend/src/DaysOff/BusinessLogic.ts
@@ -1,5 +1,22 @@
-export type AddNewDays = 
-  {
-    userId: string;
-    days: number;
-  };
+export type DaysOffEvent =
+    | {
+    type: 'AddNewDays';
+    data: {
+        userId: string;
+        amount: number;
+    };
+}
+    | {
+    type: 'AddedToTotalAmountOfLeave';
+    data: {
+        userId: string;
+        amount: number;
+    };
+}
+    | {
+    type: 'RemovedFromTotalAmountOfLeave';
+    data: {
+        shoppingCartId: string;
+        amount: number;
+    };
+};
