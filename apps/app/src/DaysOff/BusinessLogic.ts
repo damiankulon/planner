d82d09ac93a1f424ea534324bde77bab53ee6146
damiankulon <damian.kulon@gmail.com>
2024-03-28 19:46:59 +0100

    Changes

--- apps/app/src/DaysOff/BusinessLogic.ts
+++ apps/app/src/DaysOff/BusinessLogic.ts
@@ -1,36 +1,37 @@
 export type DaysOffEvent =
-    | {
+  {
     type: 'AddNewDays';
     data: {
-        userId: string;
-        amount: number;
+      userId: string;
+      amount: number;
     };
+  }
+  | {
+  type: 'moveLeaveFromPreviousCompany';
+  data: {
+    userId: string;
+    amount: number;
+  };
 }
-   | {
-    type: 'moveLeaveFromPreviousCompany';
-    data: {
-        userId: string;
-        amount: number;
-    };
+  | {
+  type: 'AddedToTotalAmountOfLeave';
+  data: {
+    userId: string;
+    amount: number;
+  };
 }
-    | {
-    type: 'AddedToTotalAmountOfLeave';
-    data: {
-        userId: string;
-        amount: number;
-    };
+  | {
+  type: 'RemovedFromTotalAmountOfLeave';
+  data: {
+    userId: string;
+    amount: number;
+  };
 }
-    | {
-    type: 'RemovedFromTotalAmountOfLeave';
-    data: {
-        userId: string;
-        amount: number;
-    };
-}
-  
-    | {
-    type: 'ExchangeAmountOfLeave';
-    data: {
-        userId: string;
-        amount: number;
-    };
+
+  | {
+  type: 'ExchangeAmountOfLeave';
+  data: {
+    userId: string;
+    amount: number;
+  };
+};
--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,14 +1,23 @@
-import express from 'express';
+import { EventStoreDBClient } from '@eventstore/db-client';
+import { Application } from 'express';
+import { getApplication, startAPI } from '@event-driven-io/emmett-expressjs';
+import { getEventStoreDBEventStore } from '@event-driven-io/emmett-esdb';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = express();
+const eventStoreDBClient = EventStoreDBClient.connectionString(
+  `esdb://localhost:2113?tls=false`
+);
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const eventStore = getEventStoreDBEventStore(eventStoreDBClient);
+
+const application: Application = getApplication({
+  apis: []
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+startAPI(application, {
+  port
 });
