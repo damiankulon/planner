50543204fa96c355568ab9272aac190db8aebf31
damiankulon <damian.kulon@gmail.com>
2024-03-31 23:28:19 +0200

    Add test

---
+++ apps/app/src/DaysOff/BusinessLogic.unit.spec.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { DeciderSpecification } from '@event-driven-io/emmett';
+import { randomUUID } from 'node:crypto';
+import { describe, it } from 'node:test';
+
+const given = DeciderSpecification.for({
+  decide,
+  evolve,
+  initialState: getInitialState,
+});
+
+describe('DayOff', () => {
+  describe('When empty user balance', () => {
+    it('Should rejected', () => {
+      given({
+        type: 'UserRequestedDayOff',
+        data: {
+          userId: userId,
+          amount:1,
+          addedAt: oldTime,
+        },
+      })
+    })
+  });
+});
+
+const oldTime = new Date();
+const now = new Date();
+const userId = randomUUID();
--- apps/app/src/DaysOff/DayOff.ts
+++ apps/app/src/DaysOff/DayOff.ts
@@ -1,10 +1,1 @@
 import type { Event } from '@event-driven-io/emmett';
-
-
-export type ProductItemAddedToShoppingCart = Event<
-  'ProductItemAddedToShoppingCart',
-  {
-    shoppingCartId: string;
-    addedAt: Date;
-  }
->;
